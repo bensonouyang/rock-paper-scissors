6ca700a2d5c767228d53cf9a0b73841d94cb6de9
bensonouyang <bensonjouyang@gmail.com>
2022-12-14 23:45:15 -0800

    add game function and include check for correct spelling

--- js/rps.js
+++ js/rps.js
@@ -33,4 +33,49 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {}
+function game() {
+  let playerscore = 0;
+  let computerscore = 0;
+  let tiescore = 0;
+  for (let i = 0; i < 5; i++) {
+    let choice = prompt("Choose rock, paper or scissors");
+    choice = choice.toLowerCase();
+    while (choice != "rock" && choice != "paper" && choice != "scissors") {
+      choice = prompt("Check your spelling, choose rock, paper or scissors");
+      choice = choice.toLowerCase();
+    }
+    let outcome = playRound(choice, getComputerChoice());
+    if (outcome == "Congratulations, you win!") {
+      playerscore++;
+      console.log("You win");
+      console.log(
+        `Player Score is ${playerscore}, Computer Score is ${computerscore}, Number of Ties is ${tiescore}`
+      );
+    } else if (outcome == "It's a tie, play again?") {
+      tiescore++;
+      console.log("It's a tie");
+      console.log(
+        `Player Score is ${playerscore}, Computer Score is ${computerscore}, Number of Ties is ${tiescore}`
+      );
+    } else {
+      computerscore++;
+      console.log("You lost");
+      console.log(
+        `Player Score is ${playerscore}, Computer Score is ${computerscore}, Number of Ties is ${tiescore}`
+      );
+    }
+  }
+  if (playerscore > computerscore) {
+    console.log(
+      `You beat the computer ${playerscore}/5 times. You won the game!`
+    );
+  } else if (playerscore == computerscore) {
+    console.log(
+      `Both you and the computer won ${playerscore}/5 times. It's a tie!`
+    );
+  } else {
+    console.log(
+      `The computer beat you ${computerscore}/5 times. You lost the game.`
+    );
+  }
+}
